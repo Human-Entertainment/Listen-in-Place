9e87370e95c5e658ed698ff7c4f0faf355516280
Bastian Inuk Christensen <bastian@inuk.blog>
2020-05-23 20:35:22 +0200

    Initial Commit

---
+++ Listen in Place/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Listen in Place
+//
+//  Created by Bastian Inuk Christensen on 23/05/2020.
+//  Copyright © 2020 Bastian Inuk Christensen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
